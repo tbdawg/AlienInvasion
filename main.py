080777f51cd90bfd68eb656b4ebecf2130e15045
tbdawg <perrocaotico@gmail.com>
2021-07-05 16:14:39 -0700

    Refactor run_game method add helper methods _check_events, _update_screen

--- main.py
+++ main.py
@@ -21,20 +21,24 @@ class AlienInvasion:
         # Add the ship
         self.ship = Ship(self)
 
+    @staticmethod
+    def _check_events():
+        # Respond to keyboard presses and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # Update images on the screen and flip to the new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blit_me()
+        pygame.display.flip()
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blit_me()
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
 
 
 if __name__ == '__main__':
